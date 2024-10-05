ead1ca2f1388a499afba41547b8e388281fb0dc7
plohish2009 <artem.rusakov.0606@mail.ru>
2024-10-05 19:14:54 +0300

    Добавил телепорт после смерти

---
+++ scripts/Hero_death.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hero_death : MonoBehaviour
+{
+    public Vector3 Teleport_Point;
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            //Destroy(gameObject);
+            gameObject.transform.position = Teleport_Point;
+        }
+    }
+}
