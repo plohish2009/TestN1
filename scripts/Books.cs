35a42694f16af1e2d30dd878ce4ecb7ab49c33fc
Ruslan <runemogame@gmail.com>
2024-10-15 17:14:10 +0300

    lfs

--- scripts/Books.cs
+++ scripts/Books.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
@@ -6,15 +7,23 @@ using UnityEngine;
 
 public class Books : MonoBehaviour
 {
+    public GameObject[] GObject;
     public int counter = 0;
+    
     private void OnTriggerEnter2D(Collider2D collision)
    {
+        GObject = GameObject.FindGameObjectsWithTag("door");
+
         if (collision.gameObject.CompareTag("Player"))
         {
             //Destroy(gameObject);
             gameObject.SetActive(false);
             counter++;
             Debug.Log(counter);
+            if (counter == 1)
+            {
+                Destroy(GObject[0]);
+            }
         }
    }
 }
